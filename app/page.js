5d31bd1bb5fe8d33fc77862faff57e3e0d4ef02c
Nakshatrrra <nakshatratrivedi44@gmail.com>
2025-02-12 12:36:32 +0530

    Initial commit from Create Next App

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Chatbot from "./components/Chatbot";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-200">
+      <Chatbot />
+    </div>
+  );
+}
